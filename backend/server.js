1765ca8ed36ffa495ff4f50558e7929cd31d9584
amey09 <ameymhatre09@gmail.com>
2023-08-21 11:32:05 +0530

    chore: installing dependencies
    This commit installs required packages and initiates an MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
+import express from "express";
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send('API is running....');
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
