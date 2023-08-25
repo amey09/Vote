d306113626b770083f5fe81c29f09723aed4611c
amey09 <ameymhatre09@gmail.com>
2023-08-25 23:15:41 +0530

    feat: add NewVoteMutation to Vote component
    This commit adds NewVoteMutation implementation allowing user's to vote a candidate.

--- frontend/src/components/Vote.jsx
+++ frontend/src/components/Vote.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Card, CardBody, CardFooter, CardHeader, Heading, Radio, Stack } from "@chakra-ui/react";
+import { useNewVoteMutation } from "../slices/candidatesApiSlice";
 
-export const Vote = () => {
+export const Vote = ({ candidates }) => {
+    const [hasVoted, setHasVoted] = useState(false);
+    const [selectedCandidate, setSelectedCandidate] = useState(null);
+    const [newVote, { isLoading }] = useNewVoteMutation();
 
-    const handleVote = async () => {
+    useEffect(() => {
+        const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+        setHasVoted(userInfo?.hasVoted || false);
+    }, []);
 
+    const handleVote = async (candidateId) => {
+
+        if (isLoading) {
+            return;
+        }
+
+        const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+        const user_email = userInfo ? userInfo.email : null;
+
+        try {
+            const mutationData = { email: candidateId, userEmail: user_email };
+            const response = await newVote(mutationData);
+
+            if (response.data && response.data.user) {
+                setSelectedCandidate(candidateId);
+                setHasVoted(true);
+
+                localStorage.setItem('userInfo', JSON.stringify({
+                    ...userInfo,
+                    hasVoted: true
+                }));
+            } else {
+                console.error("Voting failed");
+            }
+        } catch (error) {
+            console.error("Error voting:", error);
+        }
     };
+
+    if (hasVoted) {
+        return (
+            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px', marginTop: '100px' }}>
+                <Card w="400px" h="auto">
+                    <CardHeader textAlign="center">
+                        <Heading size="md">You have already voted!</Heading>
+                    </CardHeader>
+                    <CardBody>
+                        <Heading textAlign="center">Thank you for voting!</Heading>
+                    </CardBody>
+                </Card>
+            </Box>
+        );
+    }
 
     return (
         <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px', marginTop: '100px' }}>
@@ -15,15 +64,18 @@ export const Vote = () => {
                 </CardHeader>
                 <CardBody>
                     <Stack spacing={2}>
+                        {candidates.map(candidate => (
                             <Radio
-                                key={}
+                                key={candidate.email}
                                 size="lg"
                                 name="candidate"
                                 colorScheme="blue"
-                                isChecked={}
-                                onChange={() => handleVote()}
+                                isChecked={selectedCandidate === candidate.email}
+                                onChange={() => handleVote(candidate.email)}
                             >
+                                {candidate.name}
                             </Radio>
+                        ))}
                     </Stack>
                 </CardBody>
                 <CardFooter textAlign="center">
